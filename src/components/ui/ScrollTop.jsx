7da7457d29dfeecd7cfa200b2eeefd8bff1584ef
excel <axceldesigns@gmail.com>
2025-09-19 12:52:29 +0100

    profile

--- src/components/ui/ScrollTop.jsx
+++ src/components/ui/ScrollTop.jsx
@@ -5,7 +5,7 @@ export default function ScrollTop() {
   return (
     <>
       <div
-        className="fixed bottom-10 right-5 flex justify-end cursor-pointer"
+        className="fixed bottom-20 right-5 flex justify-end cursor-pointer"
         onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
       >
         <FaAngleDoubleUp className="text-3xl shadow-white" />
